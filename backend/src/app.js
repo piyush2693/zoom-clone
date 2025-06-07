7851c98755e5a3c7dd0ccbcaae2f993f8437a3cb
Piyush <pk9995114@gmail.com>
2025-06-07 18:36:00 +0530

    setup completed

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import {connectToSocket} from './controllers//socketManager.js'
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+
+app.use(cors());
+
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({ limit: "40kb", extended: true})); 
+
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://pk9995114:pk1234@cluster0.lipcgwx.mongodb.net/");
+    console.log(`Mongo connected db host : ${connectionDb.connection.host}`)
+  server.listen(app.get("port"), () => {
+    console.log("Listing on port 8000");
+  });
+};
+
+start();
